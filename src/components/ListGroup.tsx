b5a5dde4f8ffb20f7fb0bdec87e82820221d452c
Sudaisneelambra <sudaisanuneelambra21@gmail.com>
2024-06-02 15:09:07 +0530

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,37 @@
+// import {Fragment} from'react'
 function ListGroup() {
+  let items = ["apple", "orange", "grapes", "tomatto"];
+  items = [];
+
+  const condition = items.length === 0 ? <p>Not Found</p> : null
+  
+  const getItem = ()=>{
+    return items.length === 0 ? <p>Not Found In Function</p> : null
+  }
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List Of Items</h1>
+      <>
+        <h3>Item</h3>
+        {/* method One */}
+        {condition}
+
+        {/* method two */}
+        {items.length === 0 ? <p>No item Found</p> : null}  
+
+        {/* method 3 */}
+        {items.length ===0 && <p>NO ITEMS</p>}
+
+        {/* third method */}
+        {getItem()}
+      </>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
